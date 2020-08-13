98a08ab0ab05542cc9cfd5cf2f2bb41397ba3a3a
Chinmay Bag <chinmay@Chinmays-MacBook-Pro.local>
2020-08-13 20:47:21 +0530

    feat: make eventstore initialization generic and not specific to mongodb

--- src/interfaces/eventsourcing.options.ts
+++ src/interfaces/eventsourcing.options.ts
@@ -1,3 +1,16 @@
+import { supportedDatabases } from './database.config';
+
 export interface EventSourcingOptions {
     mongoURL: string;
 }
+
+export interface EventSourcingGenericOptions {
+    dialect: supportedDatabases;
+    url?: string;
+    host?: string;
+    port?: number;
+    options: {
+        ssl: boolean;
+        [key: string]: unknown;
+    }
+} 
